d304868c9ef1473622dca9e7518f4eee63be018f
darshanaswath <39420536+darshanaswath@users.noreply.github.com>
2020-07-24 22:41:40 +0530

    CRUD Operation == Success

--- controller/colleges.ts
+++ controller/colleges.ts
@@ -1,7 +1,7 @@
 import { College } from "../types.ts";
 
 // Static Data (will be moved to database later on)
-let colleges : College[] = [
+let colleges: College[] = [
   {
     id: "1",
     name: "Bangalore Institute of technology",
@@ -25,53 +25,100 @@ let colleges : College[] = [
 // GET all colleges
 // @route GET /api/v1/colleges/
 
-const getColleges = ({response}: {response: any}) =>{
-    response.body = {
-        success: true,
-        data : colleges
-    }
-}
+const getColleges = ({ response }: { response: any }) => {
+  response.body = {
+    success: true,
+    data: colleges,
+  };
+};
 
 // GET single college
 // @route GET /api/v1/colleges/:id
 
-const getCollege = ({ params , response }: { params: { id : string}, response: any }) => {
-    const college : College | undefined = colleges.find(c => c.id === params.id)
+const getCollege = (
+  { params, response }: { params: { id: string }; response: any },
+) => {
+  const college: College | undefined = colleges.find((c) => c.id === params.id);
 
-    if( college ){
-        response.status = 200
-        response.body = {
-            success : true,
-            data : college
-        }
-    } else {
-        response.status = 404
-        response.body = {
-            success : false,
-            msg : 'College not found'
-        }
-    }
-}
+  if (college) {
+    response.status = 200;
+    response.body = {
+      success: true,
+      data: college,
+    };
+  } else {
+    response.status = 404;
+    response.body = {
+      success: false,
+      msg: "College not found",
+    };
+  }
+};
 
 // ADD college
 // @route GET /api/v1/colleges/
 
-const addCollege = ({ response }: { response: any }) => {
-  response.body = "add";
+const addCollege = async (
+  { request, response }: { request: any; response: any },
+) => {
+  const body = await request.body();
+  if (!request.hasBody) {
+    response.status = 400;
+    response.body = {
+      success: false,
+      msg: "No Data found",
+    };
+  } else {
+    const college: College = body.value;
+    colleges.push(college);
+    response.status = 201;
+    response.body = {
+      success: true,
+      data: college,
+    };
+  }
 };
 
 // UPDATE college
 // @route PUT /api/v1/colleges/:id
 
-const updateCollege = ({response }: { response: any }) => {
-    response.body = 'update'
+const updateCollege = async (
+  { response, params, request }: {
+    params: { id: string };
+    request: any;
+    response: any;
+  },
+) => {
+  const college: College | undefined = colleges.find((c) => c.id === params.id);
+  if (college) {
+    const body = await request.body()
+    const updateData : {name ?: string , type ?: string , established  ?: number } = body.value
+
+    colleges = colleges.map(p => p.id === params.id ? {...p , ...updateData}: p)
+    response.status = 200;
+    response.body = {
+        success : true,
+        data : colleges
+    }
+  } else {
+    response.status = 404;
+    response.body = {
+      success: false,
+      msg: "College not found",
+    };
+  }
 };
 
 // DELETE college
 // @route DELETE /api/v1/colleges/:id
 
-const deleteCollege = ({ response }: { response: any }) => {
-    response.body = 'delete'
+const deleteCollege = ({ response , params }: { params : {id: string} ,response: any }) => {
+  colleges = colleges.filter(c => c.id !== params.id)
+    response.status = 200;
+    response.body = {
+    success: true,
+    msg : "product deleted"
+    }
 };
 
-export { getColleges,getCollege,addCollege,updateCollege,deleteCollege }
+export { getColleges, getCollege, addCollege, updateCollege, deleteCollege };
