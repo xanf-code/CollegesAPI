8326aacecd9f7fa8d86ee86c746251e57a807ee7
darshanaswath <39420536+darshanaswath@users.noreply.github.com>
2020-07-24 20:34:00 +0530

    RoutesDone

--- controller/colleges.ts
+++ controller/colleges.ts
@@ -20,3 +20,43 @@ let colleges : College[] = [
     established: 2019,
   },
 ];
+
+// GET all colleges
+// @route GET /api/v1/colleges/
+
+const getColleges = ({response}: {response: any}) =>{
+    response.body = {
+        success: true,
+        data : colleges
+    }
+}
+
+// GET single college
+// @route GET /api/v1/colleges/:id
+
+const getCollege = ({ response }: { response: any }) => {
+    response.body = 'get'
+}
+
+// ADD college
+// @route GET /api/v1/colleges/
+
+const addCollege = ({ response }: { response: any }) => {
+    response.body = 'getSingle'
+};
+
+// UPDATE college
+// @route PUT /api/v1/colleges/:id
+
+const updateCollege = ({ response }: { response: any }) => {
+    response.body = 'update'
+};
+
+// DELETE college
+// @route DELETE /api/v1/colleges/:id
+
+const deleteCollege = ({ response }: { response: any }) => {
+    response.body = 'delete'
+};
+
+export { getColleges,getCollege,addCollege,updateCollege,deleteCollege }
--- routes.ts
+++ routes.ts
@@ -1,9 +1,12 @@
 import { Router } from "https://deno.land/x/oak/mod.ts"
+import { getColleges,getCollege,updateCollege,addCollege,deleteCollege } from "./controller/colleges.ts"
 
 const router = new Router()
 
-router.get("/api/v1/colleges", ({ response }: { response: any }) => {
-  response.body = "New API"
-})
-
+router.get("/api/v1/colleges" , getColleges)
+        .get("/api/v1/colleges/:id", getCollege)
+        .put("/api/v1/colleges/:id", updateCollege)
+        .post("/api/v1/colleges/", addCollege)
+        .delete("/api/v1/colleges/:id", deleteCollege)
+        
 export default router
